06af1cf4a13db3390f45371ac0345c4c3ae60d45
jatdalf <70412644+jatdalf@users.noreply.github.com>
2024-06-10 14:48:46 -0300

    responsive logo and presentation cards

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import style from './Home.module.css'
 import Header from '../../components/Header/Header.jsx';
 import RentSaleCard from "../../components/RentSaleCard/RentSaleCard";
+import Opportunities from "../../components/Opportunities/opportunities";
 //import Carrousel from "../../components/Carrousel/Carrousel";
 //import MisionVisionValor from "../../components/MisionVisionValor/MisionVisionValor";
 //import WhatsApp from "../../components/Whatsapp/Whatsapp";
 //import Footer from "../../components/Footer/Footer";
 //import Contacto from "../Contacto/Contacto";
-//import Opportunities from "../../components/Opportunities/opportunities";
 //import CpiBanner from "../../components/CpiBanner/CpiBanner";
 //import Map from "../../components/Map/Map";
 //import OfferedProperties from "../../components/OfferedProperties/OfferedProperties";
@@ -24,8 +24,8 @@ const Home = () => {
              <div className={style.container}>
                 <div className={style.home}>
                     <RentSaleCard />
-                    {/* <Opportunities />
-                    <OfferedProperties />
+                    <Opportunities />
+                    {/* <OfferedProperties />
                     <QueresVender />    
                     <YourPlace />                          
                     <Comments />
