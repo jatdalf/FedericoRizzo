6b6edab28bf79db850df35d70070bd7def2dc705
jatdalf <70412644+jatdalf@users.noreply.github.com>
2024-08-20 11:11:44 -0300

    no Image

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -3,6 +3,7 @@ import style from './Home.module.css'
 import Header from '../../components/Header/Header.jsx';
 import RentSaleCard from "../../components/RentSaleCard/RentSaleCard";
 import Opportunities from "../../components/Opportunities/opportunities";
+import OfferedProperties from "../../components/OfferedProperties/OfferedProperties";
 //import Carrousel from "../../components/Carrousel/Carrousel";
 //import MisionVisionValor from "../../components/MisionVisionValor/MisionVisionValor";
 //import WhatsApp from "../../components/Whatsapp/Whatsapp";
@@ -10,7 +11,6 @@ import Opportunities from "../../components/Opportunities/opportunities";
 //import Contacto from "../Contacto/Contacto";
 //import CpiBanner from "../../components/CpiBanner/CpiBanner";
 //import Map from "../../components/Map/Map";
-//import OfferedProperties from "../../components/OfferedProperties/OfferedProperties";
 //import QueresVender from "../../components/QueresVender/QueresVender";
 //import YourPlace from "../../components/YourPLace/YourPlace";
 //import Comments from "../../components/Comments/Comments";
@@ -25,7 +25,8 @@ const Home = () => {
                 <div className={style.home}>
                     <RentSaleCard />
                     <Opportunities />
-                    {/* <OfferedProperties />
+                    <OfferedProperties />
+                    {/* 
                     <QueresVender />    
                     <YourPlace />                          
                     <Comments />
